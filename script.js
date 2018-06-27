34f97fc20187426c187ed65b8de52e6c6dd7323a
AvianWard101 <40601283+AvianWard@users.noreply.github.com>
2018-06-27 09:22:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ $(button).click(funtion(){
 
  })
 })
+<link rel='stylesheet'hret='index.css'>
 </script>
+src=https://code.jqery.com/jquery.com-3.3.1.min.js="
+intergity="sha256-Fgpcb/kJQLNFqu91ta320/
+cross
